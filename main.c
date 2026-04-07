6b9cdd2b2b38efe1be65d7e40cc05376521d5ef0
owhska <brunesaraujo@gmail.com>
2026-04-07 11:03:21 -0300

    commit: Analise de operadores

--- main.c
+++ main.c
@@ -3,46 +3,34 @@
 #include <string.h>
 #include "afd_numero.h"
 #include "identificador.h"
+#include "operadores.h"
 
 int main(){
 
-    char teste[][20] = {
-        "123",
-        "12.5",
-        "1.2E10",
-        "1.2E-3",
-        ".",
-        "E10",
-        "12E",
-        "+-10",
-        "x",
-        "abc123",
-        "program",
-        "var"
+    char testes[][10] = {
+        ":=",
+        "<=",
+        "<>",
+        "+",
+        "-",
+        "*",
+        "/",
+        ";",
+        "(",
+        ":",
+        "."
     };
 
-    int total = sizeof(teste) / sizeof(teste[0]);
+    int total = sizeof(testes) / sizeof(testes[0]);
 
     for (int i = 0; i < total; i++) {
+        TokenType t = reconhecerOperador(testes[i]);
 
-        if (reconhecerNumeros(teste[i])) {
-            printf("%s -> NUMERO\n", teste[i]);
-        }
-        else if (reconhecerIdentificador(teste[i])) {
-
-            TokenType t = verificarPalavraReservada(teste[i]);
-
-            if (t == ID)
-                printf("%s -> ID\n", teste[i]);
-            else
-                printf("%s -> PALAVRA RESERVADA\n", teste[i]);
-        }
-        else {
-            printf("%s -> INVALIDO\n", teste[i]);
-        }
+        if (t != ERRO)
+            printf("%s -> OPERADOR/SIMBOLO\n", testes[i]);
+        else
+            printf("%s -> INVALIDO\n", testes[i]);
     }
-
     return 0;
-
 }
 
--- token.h
+++ token.h
@@ -5,8 +5,10 @@
 typedef enum {
     KW_PROGRAM, KW_VAR, KW_INTEGER, KW_REAL,
     ID, NUM_INT, NUM_REAL,
-    OP_ASS, OP_AD, OP_MIN,
-    SMB_SEM, SMB_COM,
+    OP_ASS, OP_AD, OP_MIN, OP_EQ, OP_NE, OP_LT,
+    OP_LE, OP_GT, OP_GE, OP_MUL, OP_DIV,
+    SMB_SEM, SMB_COM, SMB_OPA, SMB_CPA,
+    SMB_COL, SMB_DOT, SMB_OBC, SMB_CBC,
     ERRO
 } TokenType;
 

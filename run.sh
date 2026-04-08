65bfdb3249b6f89a530d782e7078212768e17065
owhska <brunesaraujo@gmail.com>
2026-04-08 15:56:56 -0300

    att: run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf saida.err saida.ts saida.lex
 gcc main.c afd_numero.c identificador.c operadores.c lexer.c token.c ts.c  -o main
 ./main
 
---
+++ saida.ts
@@ -0,0 +1,5 @@
+program
+x
+y
+if
+z
